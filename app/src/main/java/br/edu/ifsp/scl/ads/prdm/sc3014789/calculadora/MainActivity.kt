1020d22c16b36dbef2ef21864f4fda02f88e1e77
isaacnwt <isaacnewton02@hotmail.com>
2025-08-25 13:22:52 -0300

    feat: funcionamento dos calculos

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/calculadora/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/calculadora/MainActivity.kt
@@ -1,20 +1,92 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.calculadora
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3014789.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private var numeroAtual = ""
+    private var primeiroNumero: Int? = null
+    private var operacao: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        with(amb) {
+            setContentView(amb.root)
+
+            initBotoesNumeros()
+            initBotoesOperacoes()
+            initClearButton()
+
+            igualBt.setOnClickListener {
+                val resultado = getResultadoOperacao()
+                visorTv.text = resultado.toString()
+                numeroAtual = resultado.toString()
+                operacao = null
+            }
+
+        }
+
+    }
+
+    private fun ActivityMainBinding.getResultadoOperacao(): Int? {
+        val segundoNumero: Int = numeroAtual.toInt()
+        return when(operacao) {
+            "/" -> primeiroNumero?.div(segundoNumero)
+            "X" -> primeiroNumero?.times(segundoNumero)
+            "-" -> primeiroNumero?.minus(segundoNumero)
+            "+" -> primeiroNumero?.plus(segundoNumero)
+            else -> {null}
+        }
+    }
+
+    private fun ActivityMainBinding.initBotoesNumeros() {
+        val botoesNumeros = listOf(
+            zeroBt, umBt, doisBt, tresBt, quatroBt, cincoBt, seisBt, seteBt, oitoBt, noveBt
+        )
+
+        botoesNumeros.forEach { botao ->
+            botao.setOnClickListener {
+                if (operacao == null) {
+                    numeroAtual += botao.text
+                    visorTv.text = numeroAtual
+                } else {
+                    numeroAtual += botao.text
+                    val operacaoCompleta = primeiroNumero.toString() + operacao + numeroAtual
+                    visorTv.text = operacaoCompleta
+                }
+            }
+        }
+    }
+
+    // TODO - tratar casos onde já tem uma operação selecionada
+    private fun ActivityMainBinding.initBotoesOperacoes() {
+        val botoesOperacoes = listOf(
+            divisaoBt, multiplicacaoBt, subtracaoBt, adicaoBt
+        )
+
+        botoesOperacoes.forEach { botao ->
+            botao.setOnClickListener {
+                primeiroNumero = numeroAtual.toInt()
+                numeroAtual = ""
+                operacao = botao.text.toString()
+
+                val operacaoParcial = primeiroNumero.toString() + operacao
+                visorTv.text = operacaoParcial
+            }
+        }
+    }
+
+    private fun ActivityMainBinding.initClearButton() {
+        clearBt.setOnClickListener {
+            visorTv.text = ""
+            numeroAtual = ""
+            primeiroNumero = null
+            operacao = null
         }
     }
 }
